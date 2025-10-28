650f899c09b7aba4e6d343e539de37ad9795aabf
jessicaobi <obicjessica@gmail.com>
2025-10-27 23:19:26 -0400

    get all, by id, and delete routes working

---
+++ NodeJS-Express/index.js
@@ -0,0 +1,107 @@
+console.log("Hello world");
+
+const express = require("express");
+const app = express();
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
+
+
+let persons = [
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+const currentTime = new Date()
+app.get('/info', (request, response) => {
+  response.send(
+    `<p>Phonebook has info for ${persons.length} people
+    </p>
+    <p>
+    ${currentTime}</p>`
+  )
+})
+
+
+app.get('/api/persons/:id', (request, response) => {
+console.log("are we getting here")
+// We had to make the request.params.id into a string because in the array that persons is declared in, the ids are also listed as strings
+  const id = String(request.params.id)
+  const person = persons.find(person => person.id === id)
+  
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = String(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
+})
+
+
+const generateId = () => {
+  const maxId = persons.length > 0
+    ? Math.max(...persons.map(n => Number(n.id)))
+    : 0
+  return String(maxId + 1)
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+
+  const person = {
+    content: body.content,
+    important: body.important || false,
+    id: generateId(),
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
+})
+
+
+
+
+
+
